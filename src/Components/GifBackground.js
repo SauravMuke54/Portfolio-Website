5e2d778d19805690ccd8979ccf1bf06503392077
Saurav Muke <98262822+SauravMuke54@users.noreply.github.com>
2025-08-01 18:49:03 +0530

    Update GifBackground.js

--- src/Components/GifBackground.js
+++ src/Components/GifBackground.js
@@ -5,7 +5,7 @@ import '../App.css'; // Create a CSS file for styling
 const GifBackground = () => {
   return (
     <div className="gif-background">
-      <img src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/9bc27292880429.5e569ff84e4d0.gif" alt="Background GIF" />
+      <img src="https://giffiles.alphacoders.com/201/201658.gif" alt="Background GIF" />
     </div>
   );
 };
